8e7e1f6aa1223b04a740c6d3491446320c2d57aa
Flávio Pavim <contato@flaviopavim.com.br>
2021-07-05 01:50:56 -0300

    Commit do teste em Java

--- src/main/java/expertostech/tutorial/rest/api/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package expertostech.tutorial.rest.api.repository;
-
-import expertostech.tutorial.rest.api.model.UsuarioModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
-}
